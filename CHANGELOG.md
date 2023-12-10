17d685d31effe21da44d2369aee7bbc3bb67ef9d
Roman Onishchenko <orm81zp@gmail.com>
2023-12-10 17:40:10 +0200

    raise up version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Change Log
 
+## 0.0.4
+
+### Features
+
+-   save empty contact book
+
 ## 0.0.3
 
 ### Features
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def get_long_description():
 
 setup(
     name='goit-assistant-bot',
-    version='0.0.3',
+    version='0.0.4',
     description='A personal console bot assistant that helps you manage your contacts and notes.',
     long_description=get_long_description(),
     long_description_content_type='text/markdown',
